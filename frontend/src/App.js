d15ae4dead27b17fb1bb50208b2039e583beb143
Fernando Leniz <ferleniz93@gmail.com>
2021-08-09 23:03:49 -0300

    pusheo del dia

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,20 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Home from './pages/Home'
-import Cities from './pages/Cities'
-import notFound404 from './pages/notFound404'
-import { Route, BrowserRouter,Switch,Redirect } from 'react-router-dom'
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
+import Cities from "./pages/Cities";
+import notFound404 from "./pages/notFound404";
+import { Route, BrowserRouter, Switch, Redirect } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-    <Switch>
-    <Route exact path="/" component={Home}/>
-    <Route path="/Cities" component={Cities}/>
-    <Route path="/notFound" component={notFound404}/>
-    <Redirect to="/notFound"/>
-    </Switch>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/cities" component={Cities} />
+        <Route path="/notFound" component={notFound404} />
+        <Redirect to="/notFound" />
+      </Switch>
     </BrowserRouter>
-    
   );
 }
 
--- frontend/src/components/TextAndVideo.js
+++ frontend/src/components/TextAndVideo.js
@@ -8,8 +8,8 @@ const TextAndVideo=() => {
         <div className="d-flex justify-content-center text-center">
         <div className="contentVideo">
             <h1 className="animate__animated animate__rubberBand">MyTinerary</h1>
-                <h2 className="animate__animated animate__zoomIn">"Find your perfect trip,
-                designed by <span>insiders</span> who know and love their cities!"
+                <h2 className="animate__animated animate__zoomIn">Find your perfect trip,
+                designed by <span>insiders</span> who know and love their cities!
                 </h2>
             </div>
         </div>
